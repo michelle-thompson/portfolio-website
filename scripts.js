6a5a44ab49b383ec834fe1baf0156466f216be98
Michelle Thompson <michelle.thompson1219@gmail.com>
2018-11-09 22:34:54 -0800

    Updated jQuery

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-console.log("Hello there!")
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
 
 $(document).ready(function() {
   $(".project-preview").on("click", function() {
